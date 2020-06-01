84540181bef7c08ceda5d32992bf2815e961d42b
Yao Yuqiang <yuqiang.yao@stud.tu-darmstadt.de>
2020-06-02 00:31:55 +0200

    Update rotatingDiscsFlow.py

--- modules/analytic/rotatingDiscsFlow.py
+++ modules/analytic/rotatingDiscsFlow.py
@@ -3,22 +3,17 @@ import scipy.special as spe
 
 
 def rotatingflow(r, z, R, H, omega):
-    '''Analytic Poiseuille-flow velocity field between two rotating plates
+    '''Analytic flow velocity field between two rotating plates
 
-    The coordinate system is located in the center of the channel
     r, z - coordinate in the wedge
-    r - radius of the wedge
+    R - radius of the wedge
     H - height of the wedge
     omega - angular velocity
 
     '''
-    # f(eta) is a sixth degree polynomial 
-    #u = -np.sqrt(omega**5/nu**3)/24*(6/h**3*z**5 - 10/h**2*z**4 + 4/h*z**3)*r
-    #v = omega*r*((0.77139*h*np.sqrt(omega/nu)-1)/h**4*z**4+(2-1.54278*h*np.sqrt(omega/nu))/h**3*z**3+0.77139*z*np.sqrt(omega/nu))
-    #w = omega**2/(12*nu)*(1/h**3*z**6 - 2/h**2*z**5 + 1/h*z**4)
-    #magU = np.sqrt(u**2 + v**2 + w**2)
 
-    zero_J1 = spe.jn_zeros(1,220)
+
+    zero_J1 = spe.jn_zeros(1,220)    #the positive zeros of the bessel function 1st degree
     beta = H/R
 
     v_phi_r = 0
